47cfd109218a69792e7b0c0dcd0eba3cf90e4352
hujian <2568788852@qq.com>
2019-12-16 16:07:20 +0800

    修复已知错误

--- hitool/src/main/java/com/base/library/base/BMvpActivity.kt
+++ hitool/src/main/java/com/base/library/base/BMvpActivity.kt
@@ -67,6 +67,7 @@ abstract class BMvpActivity<T : BPresenter> : AppCompatActivity(), BView {
             sweetAlertDialog?.changeAlertType(BSweetAlertDialog.PROGRESS_TYPE)
             sweetAlertDialog?.setTitleText("正在加载数据")
             sweetAlertDialog?.setCancelable(false)
+            sweetAlertDialog?.show()
         }
     }
 

74a1dbb71e2fe42957cfd75f961c71623991a9d5
hujian <2568788852@qq.com>
2020-01-17 16:32:36 +0800

    提高版本号

--- hitool/src/main/java/com/base/library/base/BMvpActivity.kt
+++ hitool/src/main/java/com/base/library/base/BMvpActivity.kt
@@ -54,6 +54,7 @@ abstract class BMvpActivity<T : BPresenter> : AppCompatActivity(), BView {
     override fun showDialog(loading: String?) {
         if (sweetAlertDialog != null && sweetAlertDialog!!.isShowing) {
             sweetAlertDialog?.setTitleText("正在加载数据")
+            sweetAlertDialog?.setConfirmText("")
             sweetAlertDialog?.changeAlertType(BSweetAlertDialog.PROGRESS_TYPE)
         } else {
             if (getSweetAlertDialog() != null) {
@@ -63,6 +64,7 @@ abstract class BMvpActivity<T : BPresenter> : AppCompatActivity(), BView {
             }
             sweetAlertDialog?.changeAlertType(BSweetAlertDialog.PROGRESS_TYPE)
             sweetAlertDialog?.setTitleText("正在加载数据")
+            sweetAlertDialog?.setConfirmText("")
             sweetAlertDialog?.show()
         }
     }
